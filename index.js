7011493c9baf21aa1816f16228a4fa936d5e583e
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-16 15:33:34 -0800

    Implement with alphabet-numbering

--- index.js
+++ index.js
@@ -12,14 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var alphabet = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
-
-module.exports = function(argument) {
-  var offset = ( argument - 1 )
-  var letters = ( alphabet[offset % 26] )
-  while (( offset = Math.floor(offset / 26) ) > 0) {
-    --offset
-    letters = ( alphabet[offset % 26] + letters ) }
-  return letters }
+module.exports = require('alphabet-numbering')(
+  [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ])

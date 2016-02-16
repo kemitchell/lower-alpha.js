261a04c05360ec1d7495c680d012525c4a290fdb
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-16 14:41:31 -0800

    Condense code style

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var alphabet = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-];
+  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
 module.exports = function(argument) {
-  var offset = argument - 1;
-  var letters = alphabet[offset % 26];
-  while ((offset = Math.floor(offset / 26)) > 0) {
-    --offset;
-    letters = alphabet[offset % 26] + letters;
-  }
-  return letters;
-};
+  var offset = ( argument - 1 )
+  var letters = ( alphabet[offset % 26] )
+  while (( offset = Math.floor(offset / 26) ) > 0) {
+    --offset
+    letters = ( alphabet[offset % 26] + letters ) }
+  return letters }
